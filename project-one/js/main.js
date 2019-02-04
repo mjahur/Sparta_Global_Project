4bfbff894f504fc55a13778b4b06f58d4d95f435
Mishu <mjahur@spartaglobal.com>
2019-02-04 15:50:44 +0000

    completed initial map , without functionality

--- project-one/js/main.js
+++ project-one/js/main.js
@@ -6,7 +6,7 @@ var mapArray =
 [0,1,0,0,1,0,0,0,1,0,1,0,0,0,1,0,0,1,0],
 [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
 [0,1,0,0,1,0,1,0,0,0,0,0,1,0,1,0,0,1,0],
-[0,1,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,1,0],
+[0,1,1,1,1,0,1,1,1,0,1,3,1,0,1,1,1,1,0],
 [0,0,0,0,1,0,0,0,2,0,2,0,0,0,1,0,0,0,0],
 [2,2,2,0,1,0,2,2,2,2,2,2,2,0,1,0,2,2,2],
 [2,2,2,0,1,0,2,0,0,0,0,0,2,0,1,0,2,2,2],
@@ -30,9 +30,22 @@ var blank = 2;
 var pacman = 3;
 
 //Draw map
-function drawMap() {for (var y = 0; y < mapArray.length; y++) {
+function drawMap() {
+  for (var y = 0; y < mapArray.length; y++) {
   for (var x = 0; x < mapArray[y].length; x++) {
-  };
+  if (mapArray[y][x] == 0) {
+    $(".gamebody").append("<div class='wall tile'></div>");
+  }
+  else if (mapArray[y][x] == 1) {
+    $(".gamebody").append("<div class='coin tile'></div>");
+  }
+  else if (mapArray[y][x] == 2) {
+    $(".gamebody").append("<div class='blank tile'></div>");
+  }
+  else if (mapArray[y][x] == 3) {
+    $(".gamebody").append("<div class='pacman tile'></div>");
+  }
+}
 }
 };
 
